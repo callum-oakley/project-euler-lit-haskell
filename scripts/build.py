7b22940c2606fb34fd4df4d272673be03ae5a4a0
Callum Oakley <callum@pusher.com>
2017-07-05 22:01:28 +0100

    use mustache template

--- scripts/build.py
+++ scripts/build.py
@@ -1,53 +1,23 @@
-import json, os, pypandoc
+import json, os, pypandoc, pystache
 
-title = "Project Euler Solutions"
-fonts = "https://fonts.googleapis.com/css?family=Cinzel|Source+Sans+Pro|Source+Serif+Pro|Source+Code+Pro"
-style = "style.css"
-intro = "resources/intro.md"
 problemTitles = json.loads(open("resources/titles.json").read())
 
-def html(elements):
-  return "".join(elements)
-
-def renderSolution(n):
-  return html([
-    "<div class='solution' id='{}'>".format(n),
-      "<h2>",
-        "<a class='problem-title' href='#{}'>".format(n),
-          "<span class='hash'># </span>",
-          problemTitles[n],
-        "</a>",
-        "<a class='problem-statement' href='https://projecteuler.net/problem={}'>{}</a>".format(n, n),
-      "</h2>",
-      pypandoc.convert_file(
-        "src/{}.lhs".format(n),
+print(pystache.render(open("resources/index.html.mustache").read(), {
+    "intro": pypandoc.convert_file(
+        "resources/intro.md",
         "html",
-        format="markdown+lhs",
-        extra_args=["--smart"]
-      ),
-    "</div>",
-  ])
-
-def render():
-  return html([
-    "<!doctype html>",
-    "<html>",
-    "<head>",
-      "<meta name='viewport' content='width=device-width, initial-scale=1'>",
-      "<title>{}</title>".format(title),
-      "<link href='{}' rel='stylesheet'>".format(fonts),
-      "<link href='{}' rel='stylesheet'>".format(style),
-    "</head>",
-    "<body>",
-      "<h1>{}</h1>".format(title),
-      "<div class='intro'>{}</div>".format(
-        pypandoc.convert_file(intro, "html", extra_args=["--smart"])
-      ),
-      html(renderSolution(n) for n in [
+        extra_args=["--smart"],
+    ),
+    "solutions": [{
+        "problemNumber": n,
+        "problemTitle": problemTitles[n],
+        "solutionBody": pypandoc.convert_file(
+            "src/{}.lhs".format(n),
+            "html",
+            format="markdown+lhs",
+            extra_args=["--smart"],
+        ),
+    } for n in [
         f.split(".")[0] for f in os.listdir("src") if f.endswith(".lhs")
-      ]),
-    "</body>",
-    "</html>",
-  ])
-
-print(render())
+    ]],
+}))
