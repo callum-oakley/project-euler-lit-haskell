c3bb2dae58a11f6dc5c15b9cae567d7fae076729
Callum Oakley <callum@pusher.com>
2017-07-06 12:31:58 +0100

    clean script up a little

--- scripts/build.py
+++ scripts/build.py
@@ -2,22 +2,27 @@ import json, os, pypandoc, pystache
 
 problemTitles = json.loads(open("resources/titles.json").read())
 
-print(pystache.render(open("resources/index.html.mustache").read(), {
-    "intro": pypandoc.convert_file(
-        "resources/intro.md",
+intro = pypandoc.convert_file(
+    "resources/intro.md",
+    "html",
+    extra_args=["--smart"],
+)
+
+def solutionBody(n):
+    return pypandoc.convert_file(
+        "src/{}.lhs".format(n),
         "html",
+        format="markdown+lhs",
         extra_args=["--smart"],
-    ),
+    )
+
+fileNumbers = [f.split(".")[0] for f in os.listdir("src") if f.endswith(".lhs")]
+
+print(pystache.render(open("resources/index.html.mustache").read(), {
+    "intro": intro,
     "solutions": [{
         "problemNumber": n,
         "problemTitle": problemTitles[n],
-        "solutionBody": pypandoc.convert_file(
-            "src/{}.lhs".format(n),
-            "html",
-            format="markdown+lhs",
-            extra_args=["--smart"],
-        ),
-    } for n in [
-        f.split(".")[0] for f in os.listdir("src") if f.endswith(".lhs")
-    ]],
+        "solutionBody": solutionBody(n),
+    } for n in fileNumbers],
 }))
