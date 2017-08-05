ef9c847dc9b3442491c1d5424b09a11767533b03
Callum Oakley <callum@pusher.com>
2017-08-06 00:21:24 +0100

    hlint

--- src/022.hs
+++ src/022.hs
@@ -5,7 +5,7 @@ import Data.List.Index (imap)
 -- A function to score each index–word pair is straight forward.
 
 score :: Int -> String -> Int
-score i name = (i + 1) * (sum $ map (\c -> ord c - 64) name)
+score i name = (i + 1) * sum (map (\c -> ord c - 64) name)
 
 -- Then the `imap` function from `Data.List.Index` makes short work of
 -- calculating the total.
